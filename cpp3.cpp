25bc76c67f4ef05725154b81a50e27c6d468066f
anubhav100rao <anubhav100rao@gmail.com>
2020-11-07 15:35:16 +0530

    second commit for trial purpose only

---
+++ cpp3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"please enter any number \n";
+    cin>>num;
+    for(int i = 0;i<num;i++){
+        cout<<i<<" ";
+    }
+    return 0;
+}
